56ec6de18e720544026297e86f52727f26a6cbdd
Jochen Leeder <78385433+bydb@users.noreply.github.com>
2024-12-10 19:57:09 +0100

    Listen angepasst

--- 500Listen.py
+++ 500Listen.py
@@ -1,4 +1,43 @@
 
+#Was ist eine Liste?
+
+	#•	Eine Liste ist ein geordnetes, veränderbares Daten­struktur-Objekt in Python.
+	#•	Sie kann verschiedene Datentypen enthalten (Strings, Zahlen, sogar andere Listen).
+
+
+# Beispiele
+
+meine_liste = ["Apfel", "Banane", "Kirsche"]
+zahlen_liste = [1, 2, 3, 4, 5]
+gemischt = ["Text", 42, True]
+
+
+print(meine_liste[0])  # "Apfel"
+print(meine_liste[-1]) # Letztes Element: "Kirsche"
+
+
+meine_liste[1] = "Birne"
+print(meine_liste) # ["Apfel", "Birne", "Kirsche"]
+
+
+meine_liste.append("Orange")      # Fügt am Ende hinzu
+meine_liste.insert(1, "Melone")   # Fügt an bestimmter Stelle hinzu
+
+#Entfernen von Elementen
+
+meine_liste.remove("Apfel") # entfernt erstes Vorkommen von "Apfel"
+letztes_element = meine_liste.pop() # entfernt und gibt das letzte Element zurück
+
+# Nützliche Methoden
+
+print(len(meine_liste))  # Länge der Liste
+meine_liste.sort()       # Sortiert die Liste (wenn Datentypen vergleichbar sind)
+meine_liste.reverse()    # Dreht die Reihenfolge um
+
+
+
+
+
 Vater = input(str("Bitte gebe den Vornamen deines Vaters ein: "))
 Mutter = input(str("bitte gebe den Vornamen deiner Mutter ein: "))
 
