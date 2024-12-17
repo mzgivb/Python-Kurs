b8d6a18440c37ac5df3f1625bec54d4ff7829aa9
Jochen Leeder <78385433+bydb@users.noreply.github.com>
2024-12-17 20:00:51 +0100

    ändern

--- 400Operatoren.py
+++ 400Operatoren.py
@@ -11,3 +11,15 @@ print (d)
 e = a / b
 
 print (e)
+
+# Beispiel Vergleichsoperatoren
+
+alter = 20
+punkte = 120
+
+if alter >= 18 and punkte >= 100:
+    print("Teilnahme erlaubt")
+else:
+    print("Teilnahme nicht erlaubt")
+
+
