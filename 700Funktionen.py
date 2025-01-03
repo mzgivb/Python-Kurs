82ef42d48ec24ec3519f68d87fea198ecf3d7b92
mzgivb <jochen.leeder@mzgivb.de>
2025-01-03 10:57:38 +0100

    funktionen

--- 700Funktionen.py
+++ 700Funktionen.py
@@ -1,14 +1,13 @@
-def auto():
-    print ("Audi 80")
+def auto(): # hier wird die Funktion definiert
+    print ("Audi 80") # hier wird der Inhalt der Funktion ausgeführt
 
-
-auto()
+auto() # hier wird die Funktion aufgerufen
 
 
 def addieren(a,b):
-    return a + b
+    return a + b # hier wird das Ergebnis der Addition zurückgegeben
 
-ergebnis = addieren(3,5)
+ergebnis = addieren(3,5) # hier wird die Funktion aufgerufen und das Ergebnis in der Variable ergebnis gespeichert
 print (ergebnis)
 
 
